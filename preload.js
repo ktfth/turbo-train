c6c531b74e3897a7f70c7cd16c2ffd4dd2b3142b
Kaique da Silva <kaeyosthaeron@gmail.com>
2020-02-25 21:29:15 -0300

    Included the logic of ips

--- preload.js
+++ preload.js
@@ -1,12 +1,16 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
+const network = require('network');
+
 window.addEventListener('DOMContentLoaded', () => {
   const replaceText = (selector, text) => {
     const element = document.getElementById(selector)
     if (element) element.innerText = text
   }
 
-  for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type])
+  for (const type of ['public', 'private', 'gateway']) {
+    network['get_' + type + '_ip']((err, ip) => {
+      replaceText(`${type}-ip`, (err || ip))
+    });
   }
 })
